a39fb3a4272f3b2aaf12dfda7339af48e583adc6
Lukas Kastern <work@lukaskastern.com>
2026-06-11 22:13:33 +0200

    update to 0.16.0

--- build.zig
+++ build.zig
@@ -1,18 +1,11 @@
 const std = @import("std");
 
+// Add vulkan include paths to module
+pub fn addIncludePaths(dep_b: *std.Build, module: *std.Build.Module) void {
+    module.addIncludePath(dep_b.path("include/vk_video"));
+    module.addIncludePath(dep_b.path("include/vulkan"));
+}
+
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const lib = b.addStaticLibrary(.{
-        .name = "vulkan-headers",
-        .root_source_file = b.addWriteFiles().add("empty.c", ""),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    inline for (.{ "vk_video", "vulkan" }) |subdir| {
-        lib.installHeadersDirectory(b.path("include/" ++ subdir), subdir, .{});
-    }
-    b.installArtifact(lib);
+    _ = b; // autofix
 }
